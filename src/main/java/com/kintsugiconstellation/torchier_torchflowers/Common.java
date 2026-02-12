5e978662171c5e024e5d2b1d1e2cc2bbabc84963
verdantserene <redalenfraser1@gmail.com>
2026-02-12 17:25:33 -0600

    Common

--- src/main/java/com/kintsugiconstellation/torchier_torchflowers/Common.java
+++ src/main/java/com/kintsugiconstellation/torchier_torchflowers/Common.java
@@ -31,8 +31,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod {
+@Mod(Common.MODID)
+public class Common {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "torchier_torchflowers";
     // Directly reference a slf4j logger
@@ -64,7 +64,7 @@ public class ExampleMod {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer) {
+    public Common(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
