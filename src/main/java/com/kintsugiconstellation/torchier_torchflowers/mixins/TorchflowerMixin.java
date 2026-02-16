4c8e47dc7720361368e84850ee59014013fc0612
verdantserene <redalenfraser1@gmail.com>
2026-02-16 16:40:41 -0600

    random bullshit, go

---
+++ src/main/java/com/kintsugiconstellation/torchier_torchflowers/mixins/TorchflowerMixin.java
@@ -0,0 +1,9 @@
+package com.kintsugiconstellation.torchier_torchflowers.mixins;
+
+import net.minecraft.world.level.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Blocks.class)
+public class TorchflowerMixin {
+
+}
