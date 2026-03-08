a86c0358ebcf4a34ea81d0c298de5ffdd8b5b03e
verdantserene <redalenfraser1@gmail.com>
2026-03-07 18:25:44 -0600

    THEY GLOW

--- src/main/java/com/kintsugiconstellation/torchier_torchflowers/mixin/TorchflowerMixin.java
+++ src/main/java/com/kintsugiconstellation/torchier_torchflowers/mixin/TorchflowerMixin.java
@@ -1,9 +1,25 @@
 package com.kintsugiconstellation.torchier_torchflowers.mixin;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FlowerBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.extensions.IBlockExtension;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(Blocks.class)
-public class TorchflowerMixin {
+@Mixin(FlowerBlock.class)
+public class TorchflowerMixin extends Block {
+    public TorchflowerMixin(Properties properties) {
+        super(properties);
+    }
 
+    @Override
+    public int getLightEmission(BlockState state, BlockGetter level, BlockPos pos) {
+        if (this.asBlock() == Blocks.TORCHFLOWER)
+            return 14;
+        else
+            return super.getLightEmission(state, level, pos);
+    }
 }
