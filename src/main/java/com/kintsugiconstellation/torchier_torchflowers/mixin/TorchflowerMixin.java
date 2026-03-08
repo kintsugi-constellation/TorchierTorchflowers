88b5410f6cbc0ca4c7c9536c0367c180ce8356fa
verdantserene <redalenfraser1@gmail.com>
2026-03-07 18:26:13 -0600

    removed an unused import

--- src/main/java/com/kintsugiconstellation/torchier_torchflowers/mixin/TorchflowerMixin.java
+++ src/main/java/com/kintsugiconstellation/torchier_torchflowers/mixin/TorchflowerMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.neoforge.common.extensions.IBlockExtension;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(FlowerBlock.class)
